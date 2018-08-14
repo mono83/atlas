9113c7fe84e5f598267d852a367e236c405d84af
Gotterdemarung <gotterdemarung@gmail.com>
2018-08-14 10:21:58 +0300

    sorting order moved

--- query/sorting.go
+++ query/sorting.go
@@ -6,3 +6,13 @@ type Sorting interface {
 
 	GetType() SortOrder
 }
+
+// SortOrder specifies collection ordering
+type SortOrder byte
+
+// List of defined order types
+const (
+	UnknownSort SortOrder = 0
+	Asc         SortOrder = 1
+	Desc        SortOrder = 2
+)
